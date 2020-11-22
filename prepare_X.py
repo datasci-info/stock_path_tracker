10ce747c9e95976d3528fb50759fa08f4da9538e
cyyen <yen.chiayi@gmail.com>
2020-11-22 14:45:59 +0800

    add lag value

--- prepare_X.py
+++ prepare_X.py
@@ -1,28 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
 
-# In[1]:
-
-
+# %%
 from findataflow.dataprocs import feature_generator as fg
 from findataflow.dataprocs import resample as resampler
 
-
-# In[2]:
-
-
+# %%
 import pandas as pd
 
-
-# In[3]:
-
-
+# %%
 specs = []
 
-
-# In[4]:
-
-
+# %%
 # BB
 FUNCS = {
     'price': ['BULL_MA', 'BULL_UP', 'BULL_DOWN'],}
@@ -47,9 +48,7 @@ specs += [
     ]
 
 
-# In[5]:
-
-
+# %%
 # BIAS
 FUNCS = {
     'swig': ['BIAS'],
@@ -73,9 +72,7 @@ specs += [
     ]
 
 
-# In[6]:
-
-
+# %%
 # candlestick
 FUNCS = {
     'price': ['candlestick'],
@@ -99,9 +96,7 @@ specs += [
     ]
 
 
-# In[7]:
-
-
+# %%
 # CDP
 FUNCS = {
     'price': [
@@ -127,9 +122,7 @@ specs += [
     ]
 
 
-# In[8]:
-
-
+# %%
 # DMI 
 FUNCS = {
     'swig': [
@@ -155,9 +148,7 @@ specs += [
     ]
 
 
-# In[9]:
-
-
+# %%
 # KD
 FUNCS = {
     'swig': [
@@ -186,9 +177,7 @@ specs += [
     ]
 
 
-# In[10]:
-
-
+# %%
 # MACD
 FUNCS = {
     'swig': [
@@ -217,9 +206,7 @@ specs += [
     ]
 
 
-# In[11]:
-
-
+# %%
 # MFI
 FUNCS = {
     'swig': ['MFI'],
@@ -243,9 +230,7 @@ specs += [
     ]
 
 
-# In[12]:
-
-
+# %%
 # OBV
 FUNCS = {
     'vol': ['OBV'],
@@ -268,9 +253,7 @@ specs += [
     ]
 
 
-# In[13]:
-
-
+# %%
 # RSI
 FUNCS = {
     'swig': ['RSI'],
@@ -293,9 +276,7 @@ specs += [
     ]
 
 
-# In[14]:
-
-
+# %%
 # SAR
 FUNCS = {
     'swig': ['SAR'],
@@ -322,9 +303,7 @@ specs += [
     ]
 
 
-# In[15]:
-
-
+# %%
 # SMA
 FUNCS = {
     'price': ['SMA'],
@@ -348,22 +327,34 @@ specs += [
     ]
 
 
-# In[20]:
-
-
+# %%
 def get_single_feature(spec):
-    df_feature = resampler.get_OHLCV_given_frequency(spec['symbol'], spec['freq']).pipe(fg.generate_root_feature, spec=spec)
+    df_feature = (
+        resampler.get_OHLCV_given_frequency(spec['symbol'], spec['freq']).
+        pipe(fg.generate_root_feature, spec=spec)
+    )
     return df_feature
 
-def get_featrues(specs):
-    df_features = pd.concat([get_single_feature(spec) for spec in specs], axis=1)
+def get_featrues(specs, nlags=5):
+    df_features = pd.concat([
+        get_single_feature(spec).pipe(fg.extend_by_nlags, spec='', nlags=nlags)
+        for spec in specs
+    ], axis=1)
+    
+    colnames = []
+    for idx_spec in range(len(specs)):
+        colnames += list(map(lambda x: f'{idx_spec}_{x}', df_features.columns[idx_spec*nlags: (idx_spec+1)*nlags]))
+    df_features.columns = colnames
+    
     return df_features
 
 
-# In[ ]:
-
+# %%
 if __name__ == '__main__':
-    df_features = get_featrues(specs)
-    df_features.shape
-    assert df_features.shape[1] == len(specs)
+    nlags=5
+    df_features = get_featrues(specs, nlags)
+    assert df_features.shape[1] == len(specs) * nlags
 
+
+
+# %%
